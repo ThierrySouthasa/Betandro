5b1fbcc84cd632c439d85abb117970b313a191a0
Thierry Southasa <southasa.thierry@gmail.com>
2025-09-26 22:24:47 +0200

    chore: 🔧 Update Next.js configuration to enable strict mode, SWC minification, and adjust TypeScript and ESLint settings

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+  typescript: {
+    ignoreBuildErrors: true
+  },
+  eslint: {
+    ignoreDuringBuilds: false
+  }
 };
 
 export default nextConfig;
